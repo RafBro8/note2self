d27150e13b236af058ae927da9dc8704b34558a1
RafBro8 <rromn8@gmail.com>
2018-03-01 21:54:57 -0600

    added form, react bootstrap, css styling, and set state in App

--- components/App.js
+++ components/App.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            text: '',
+            notes: []
+        }
+    }
+
+
+    submit() {
+        const notes = this.state.notes;
+        const newNote = { text: this.state.text };
+
+        notes.push(newNote);
+
+        this.setState({ notes: notes });
+    }
+
     render() {
+
         return (
             <div>
                 <h2>Note2Self</h2>
+                <Form inline>
+                    <FormControl onChange={event => this.setState({ text: event.target.value })}/>
+                    {' '}
+                    <Button onClick={() => this.submit()}>Submit</Button>
+                </Form>
+                {
+                    this.state.notes.map((note, index) => {   //index helps with React Warnings that each child in an array should have a unique key
+                        return (
+                            <div key={index}>{note.text}</div>
+                        );
+                    })
+                }
             </div>
         )
     }
 }
-
 
 export default App;
